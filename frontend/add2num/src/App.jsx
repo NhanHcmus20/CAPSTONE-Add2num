668f31e6f158f80a0cfed62d318f87eeac745ca4
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2024-01-15 21:24:27 +0700

    update add2num function in js

--- frontend/add2num/src/App.jsx
+++ frontend/add2num/src/App.jsx
@@ -8,6 +8,27 @@ function App() {
   const [second, setSecond] = useState(" ")
   const [res, setRes] = useState(" ")
 
+  let add2number = (num1, num2) => {
+    let carry = 0
+    let sum = ""
+
+    let i = num1.length - 1
+    let j = num2.length - 1
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+      const digit1 = i >= 0 ? parseInt(num1[i--]) : 0
+      const digit2 = j >= 0 ? parseInt(num2[j--]) : 0
+
+      const currentSum = digit1 + digit2 + carry
+      carry = Math.floor(currentSum / 10)
+      const currentDigit = currentSum % 10
+
+      // Prepend the current digit to the result
+      sum = currentDigit + sum
+    }
+
+    return sum;
+  }
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log("call api to calculate")
